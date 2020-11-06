1c971a18fad74e1fc59ec7702408cd4c011557db
Mateusz Dudulsk <mateusz.dudulski@altalog.pl>
2020-11-06 20:55:39 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import Result from "./Result";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Form/>
+
+    <Result/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Result = () => {
+    return (
+
+    <div> Miasto, pogoda... </div>
+
+    );
+
+}
+
+export default Result;
